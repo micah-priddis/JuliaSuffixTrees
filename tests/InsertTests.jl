160e9b9ebee1d85f75211a33da18f5afad3e3377
Priddis, Micah Lee <micah.priddis@wsu.edu>
2020-12-26 20:57:07 -0800

    Added example testfile to demonstrate unit testing in Julia.

---
+++ tests/InsertTests.jl
@@ -0,0 +1,12 @@
+"""
+This file contains tests for the insert function.
+
+"""
+
+using Test
+
+@testset "Insert function" begin
+    @test true == true #Example test, replace with a real 
+    #                   test once the function is implemented
+
+end
