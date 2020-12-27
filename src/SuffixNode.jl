fd75213079550beb3d4077b09f3a15179ca0e53b
Priddis, Micah Lee <micah.priddis@wsu.edu>
2020-12-27 11:51:43 -0800

    Changed the struct for SuffixNode to have a substring type, rather than store indexes of the first and last characters. The references to child and sibling nodes have been changed to Union types to allow them to be undefined (signaling the end of a path in the tree).

--- src/SuffixNode.jl
+++ src/SuffixNode.jl
@@ -3,18 +3,16 @@ The SuffixNode struct represents the nodes to be used within
 the suffix tree. These nodes will be used to form a compressed trie
 to hold all unique suffixes of a string. Conceptually, each node
 can be thought of as containing a substring. However, to avoid storing
-redundant strings, each substring is represented by two indexes pointing
-to the first and last characters in the substrings.
+redundant strings, each substring is stored as the Julia type, "SubString"
+which references the original string to reduce memory usage.
 
 
 
 """
 
 mutable struct SuffixNode
-    first::Unsigned #Index of the first letter in the node's string
-    last::Unsigned #Index of the last letter in the node's string
-    len::Unsigned # len = last - first + 1
-    child::SuffixNode
-    sibling::SuffixNode
+    str::SubString
+    child::Union{SuffixNode, Nothing}
+    sibling::Union{SuffixNode, Nothing}
     
 end
